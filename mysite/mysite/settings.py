ed51dbbc4f4aa25c7dcb3923934007af7c7547dc
tlarbgus89 <48449706+tlarbgus89@users.noreply.github.com>
2019-03-12 03:30:54 +0900

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't7(-!z)2h^6=7)+$6oxz+b4t)c_sx^1n)gtapg^91til)0qa%m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
